b7f747346802fd6cb4c010aaa9cd683024217317
kangdroid <hyunwoo286@gmail.com>
2022-01-31 15:29:51 +0900

    Initial Commit

---
+++ src/main/kotlin/com/lws/admin/lwsadmin/LwsAdminApplication.kt
@@ -0,0 +1,11 @@
+package com.lws.admin.lwsadmin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LwsAdminApplication
+
+fun main(args: Array<String>) {
+    runApplication<LwsAdminApplication>(*args)
+}
---
+++ src/test/kotlin/com/lws/admin/lwsadmin/LwsAdminApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lws.admin.lwsadmin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LwsAdminApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
